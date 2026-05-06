3aed161f3032faf18db199b2334c428e9314f60f
rjcb-commits <rayjack.cb@gmail.com>
2026-05-06 15:42:01 -0400

    chore: bump version to 1.0.1 (versionCode 5)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.ninersudoku"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.0.0"
+        versionCode = 5
+        versionName = "1.0.1"
     }
 
     signingConfigs {
