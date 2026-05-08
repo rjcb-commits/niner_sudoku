f6ece11617756290163650eaa002c04f3997de4f
rjcb-commits <rayjack.cb@gmail.com>
2026-05-07 23:59:41 -0400

    chore: bump version to 1.0.3 (versionCode 7)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.ninersudoku"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "1.0.2"
+        versionCode = 7
+        versionName = "1.0.3"
     }
 
     signingConfigs {
